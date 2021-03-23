d4abfd2fbeb7effdd76329ff5f73f68a24af5ff2
Axel Igor Aviloff <axel.aviloff@CTNDELLGFZVJB3.pampa.compasso>
2021-03-23 10:59:47 -0300

    Adicionando arquivos iniciais

---
+++ entrevista/src/test/java/com/compassouol/entrevista/EntrevistaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compassouol.entrevista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntrevistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
